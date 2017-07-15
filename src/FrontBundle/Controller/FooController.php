a6fc17247cc0214866ed18db522d25c87da8e8f7
xujiajun <me@xujiajun.cn>
2017-07-15 15:09:41 +0800

    add example of controller use

---
+++ src/FrontBundle/Controller/FooController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TastPHP\FrontBundle\Controller;
+
+use TastPHP\Common\Controller;
+
+class FooController extends Controller
+{
+    public function indexAction()
+    {
+        echo "hello FooController Action ~";
+    }
+}
