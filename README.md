c8589bbecbcda68127a8206c2bdb9ffe62e45efb
xujiajun <me@xujiajun.cn>
2017-07-19 17:01:10 +0800

    add Requests demo

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # tastphp-docs-demo
 The demo of tastphp documents
-
-> this  Repository current use Tastphp Version 1.3.1
--- src/FrontBundle/Controller/FooController.php
+++ src/FrontBundle/Controller/FooController.php
@@ -2,12 +2,21 @@
 
 namespace TastPHP\FrontBundle\Controller;
 
+use Psr\Http\Message\ServerRequestInterface;
 use TastPHP\Common\Controller;
+use TastPHP\Framework\Adapter\RequestAdapter;
+use TastPHP\Framework\Http\Request;
 
 class FooController extends Controller
 {
-    public function indexAction()
+    public function indexAction(ServerRequestInterface $request)
     {
-        echo "hello FooController Action ~";
+//        dump($request->query->get('name'));
+//        dump($request->request->get('name'));
+//        dump($request->getQueryParams());
+//        echo "hello FooController Action ~";
+        dump($request);
+        $symfonyRequest = RequestAdapter::convertSymfonyRequest($request);
+        dump($symfonyRequest);
     }
 }
