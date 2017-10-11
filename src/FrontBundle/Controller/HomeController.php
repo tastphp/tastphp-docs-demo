f9a285eacc4c16b6abe3fe8eaaf072b863d6a973
xujiajun <me@xujiajun.cn>
2017-10-11 14:08:50 +0800

    add response demo

--- src/FrontBundle/Controller/HomeController.php
+++ src/FrontBundle/Controller/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace TastPHP\FrontBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use TastPHP\Common\Controller;
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
-        echo "<br> indexAction ~";
-        return $this->render('home/index.html.twig');
+        echo "<br> indexAction ~<br>";
+//        return new JsonResponse(['name1'=>'tastphp~',"name2"=>"tastphp!"]);
+        return $this->response("response ok!");
+//        return $this->render('home/index.html.twig');
     }
 }
